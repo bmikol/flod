618af4e10533cde602e9b04e0bfda560a41b14c3
Brian Mikol <github@sffogworks.com>
2022-05-07 10:03:05 -0700

    Added location and file format prefs & path auto-create

--- flod.sh
+++ flod.sh
@@ -1,22 +1,43 @@
 #!/bin/bash
 #
 # flod, a simple time-tracking shell script
-#
 # Copyright (C) 2022 Brian Mikol
-# VERSION: 2022.0
-# 	* Initial release, based upon a shell script shared in the comments of the Lifehacker post, [Geek to Live: Quick-log your work day](https://lifehacker.com/geek-to-live-quick-log-your-work-day-189772)
+# https://github.com/bmikol/flod
+#
+# VERSION: 2022.1
+# 	* Added configuration for log location and auto-generate the path if it doesn't exist already
+#	* Added option to change between a monthly log file vs daily log files
+#
+# === LICENSE STATEMENT ===
 #
 # This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3 of the License.
 #
 # This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-read -p "What are you working on?: " TASK
+#
+# === END OF LICENSE STATEMENT ===
 
 THISMONTH="`date +"%Y%m"`"
 TODAY="`date +"%Y.%m.%d"`"
 TIME="`date +"%H:%M:%S"`"
 
-# Be sure to either create the "logs" directory in your home directory, or update the path as desired.
-echo "$TODAY @ $TIME – $TASK" >> $HOME/logs/$THISMONTH-log.txt
+# === EDIT FILE LOCATION & FILE DOCUMENT PREFERENCE BELOW ===
+
+# The log directory for flod (this should be an absolute path)
+# LOG_DIR="/Users/brian/Documents/logs"
+LOG_DIR="$HOME/logs"
+
+# The log file
+# Default is to have monthly files:
+LOG_FILE="$LOG_DIR/$THISMONTH-log.txt"
+# If you would prefer daily files, comment out the above and uncomment the below:
+# LOG_FILE="$LOG_DIR/$TODAY-log.txt"
+
+# == EDIT FILE LOCATION & FILE DOCUMENT PREFERENCE ABOVE ===
+
+mkdir -p $LOG_DIR
+
+read -p "What are you working on?: " TASK
+
+echo "$TODAY @ $TIME – $TASK" >> $LOG_FILE
