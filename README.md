9d25d234c374a9e9288d49c467670e1b4c70141b
Brian Mikol <github@sffogworks.com>
2022-05-09 20:26:46 -0700

    Added shortcuts through parameters and switched to zsh

--- README.md
+++ README.md
@@ -1,14 +1,55 @@
 # Flod
 
-A simple shell script to track the time you spend on tasks, based upon a shell shared in the comments on Gina Trapani's Lifehacker article [Geek to Live: Quick-Log Your Work Day](https://lifehacker.com/geek-to-live-quick-log-your-work-day-189772).
+A simple shell script to track the time you spend on tasks, based upon a script shared in the comments on Gina Trapani's Lifehacker article [Geek to Live: Quick-Log Your Work Day](https://lifehacker.com/geek-to-live-quick-log-your-work-day-189772).
 
 ## Future Plans
 
-* Add ability to quickly submit your tasks as an argument
-* Add shortcuts for tasks you repeatedly submit
 * Create a companion script to parse and report total time spent on projects
 
+## How To: Add More Shortcut Options
+
+To add more shortcut options, there are a couple things you need to do to make sure everything works as expected:
+
+1. Update the associative array ("declare -A shortcut…") to include your new option.
+2. Update the code that provides the different options (two different places in the "while getopts…" loop).
+3. Add your new shortcut definition.
+4. (Optional) Update the help (contents of "OPTIONS_HELP=" section) to document your new shortcut.
+
+Example:
+To add a new shortcut using the "-i" parameter that will enter the task "Buy ice cream", do the following:
+ 
+1. Update the line that starts with "declare -A shortcut" to add the new "i" option at the end:
+
+```zsh
+declare -A shortcut=([e]=false [s]=false [p]=false [h]=false [i]=false)
+```
+
+2. Updating the code that provides the different options:
+   * Update the "while getopts…" loop line to add the "i" option after the "h" option:
+
+	```zsh
+	while getopts ":esp:hi" OPTION; do
+	```
+ 
+   * Update the line AFTER the one that reads "case $option in" to include the "|i" right before the ")":
+
+	```zsh
+	 		e|s|p|h|i) shortcut[$OPTION]=true ;;
+	``` 		
+
+3. Add the new shortcut definition at the end of the "=== SHORTCUT DEFINITIONS ===" block:
+
+```zsh 
+${shortcut[i]} && echo "$TODAY @ $TIME – Buy ice cream" >> $LOG_FILE && exit 0
+```
+
 ## Change Log
+
+### v2022.02
+* Switched to a zsh script (macOS default bash version doesn't support associative arrays)
+* Added shortcut options, including help
+* Added ability to define task as a script argument for quicker entry
+* Retained original Q&A behavior as a fallback if options or an argument aren't provided
 
 ### v2022.01
 
--- flod.sh
+++ flod.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
+#!/usr/local/bin/zsh
 #
 # flod, a simple time-tracking shell script
 # Copyright (C) 2022 Brian Mikol
 # https://github.com/bmikol/flod
 #
-# VERSION: 2022.1
-# 	* Added configuration for log location and auto-generate the path if it doesn't exist already
-#	* Added option to change between a monthly log file vs daily log files
+# VERSION: 2022.2
+#	* Switched to a zsh script (macOS default bash version doesn't support associative arrays)
+# 	* Added shortcut options, including help
+#	* Added ability to define task as a script argument for quicker entry
+# 	* Retained original Q&A behavior as a fallback if options or an argument aren't provided
 #
 # === LICENSE STATEMENT ===
 #
@@ -14,15 +16,36 @@
 #
 # This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 #
 # === END OF LICENSE STATEMENT ===
+
+OPTIONS_HELP="
+Command line options:
+	-s 		Shortcut to log 'Task +Project1'
+	-e 		Shortcut to log 'Activity end'
+	-p PROJECT	Shortcut to log your task suffixed with +PROJECT
+	-h		Print this help menu
+	
+Examples:
+	To quickly log 'Task +Project1':
+		flod.sh -s
+	
+	To log a new task without a project:
+		flod.sh 'This is a task'
+	
+	To log a new task with a project (either will work and have the same result):
+		flod.sh 'This is a task +Project1'
+		flod.sh -p Project1 'This is a task'
+
+If you execute flod.sh without a valid option or argument, it will ask you what you're working on in order to log your response.
+"
 
 THISMONTH="`date +"%Y%m"`"
 TODAY="`date +"%Y.%m.%d"`"
 TIME="`date +"%H:%M:%S"`"
 
-# === EDIT FILE LOCATION & FILE DOCUMENT PREFERENCE BELOW ===
+# === EDIT FILE LOCATION & FILE DOCUMENT PREFERENCES ===
 
 # The log directory for flod (this should be an absolute path)
 # LOG_DIR="/Users/brian/Documents/logs"
@@ -34,10 +57,35 @@ LOG_FILE="$LOG_DIR/$THISMONTH-log.txt"
 # If you would prefer daily files, comment out the above and uncomment the below:
 # LOG_FILE="$LOG_DIR/$TODAY-log.txt"
 
-# == EDIT FILE LOCATION & FILE DOCUMENT PREFERENCE ABOVE ===
+# == END OF EDIT FILE LOCATION & FILE DOCUMENT PREFERENCES ===
 
 mkdir -p $LOG_DIR
 
-read -p "What are you working on?: " TASK
+if [[ ${#} -eq 0 ]]; then
+  	echo -n "What are you working on? " 
+  	read TASK
+ 	echo "$TODAY @ $TIME – $TASK" >> $LOG_FILE
+ 	exit 0
+fi
 
-echo "$TODAY @ $TIME – $TASK" >> $LOG_FILE
+declare -A SHORTCUT=([e]=false [s]=false [p]=false [h]=false)
+
+while getopts ":esp:h" OPTION; do 
+    case $OPTION in
+        e|s|p|h) SHORTCUT[$OPTION]=true ;;
+        ?) echo "\nError: '-$OPTARG' option not available."; echo "$OPTIONS_HELP"; exit 1;;
+    esac
+done
+shift "$((OPTIND-1))"
+
+# === SHORTCUT DEFINITIONS ===
+# See README for guidance on adding more options.
+
+${SHORTCUT[e]} && echo "$TODAY @ $TIME – Activity end" >> $LOG_FILE && exit 0
+${SHORTCUT[s]} && echo "$TODAY @ $TIME – Task +Project1" >> $LOG_FILE && exit 0
+${SHORTCUT[p]} && echo "$TODAY @ $TIME - $1 +${OPTARG}" >> $LOG_FILE && exit 0
+${SHORTCUT[h]} && echo "$OPTIONS_HELP" && exit 1
+
+# === END SHORTCUT DEFINITIONS ===
+
+echo "$TODAY @ $TIME - $1" >> $LOG_FILE
